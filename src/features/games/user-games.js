c4d04d7772f7ed59d3ae2d140474b63592a93ea5
Niklas Bergkvist <niklasbergkvist@mac-nikber01.mynet>
2018-12-26 14:59:53 +0100

    minor style fixes

--- src/features/games/user-games.js
+++ src/features/games/user-games.js
@@ -5,16 +5,20 @@ import { getCompareLoadingState } from "./selectors";
 
 class UserGames extends PureComponent {
   render() {
-    const {
-      compareGames,
-      startCompare,
-      removeFromCompare,
-      getCompareLoadingState
-    } = this.props;
+    const { compareGames, getCompareLoadingState } = this.props;
     const gameList = compareGames.map(game => {
+      const { appid } = game;
+      const sectionStyle = {
+        height: "100%",
+        width: "100%",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundImage: `url(https://steamcdn-a.akamaihd.net/steam/apps/${appid}/header.jpg)`
+      };
       return (
         <div key={game.appid} className="single-game">
           <span>{game.name}</span>
+          <div style={sectionStyle} />
         </div>
       );
     });
