43c051143d3f5ac47d589f7431feb338541a8b71
nbergkvist <niklasbergkvist@mac-nikber01.mynet>
2019-01-06 13:57:09 +0100

    changed http to https

--- src/features/helper/api-constants.js
+++ src/features/helper/api-constants.js
@@ -1,11 +1,11 @@
 export default {
   BASE_STEAM_GET_FRIENDS:
-    "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=A22C81E13075DA81B066A63FFA475674&steamid=",
+    "https://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=A22C81E13075DA81B066A63FFA475674&steamid=",
   BASE_STEAM_GET_GAMES:
-    "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=A22C81E13075DA81B066A63FFA475674&steamid=",
+    "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=A22C81E13075DA81B066A63FFA475674&steamid=",
   BASE_STEAM_FRIEND: "&relationship=friend",
   steamapikey: "A22C81E13075DA81B066A63FFA475674",
   STEAM_GET_ALL_GAMES: "https://api.steampowered.com/ISteamApps/GetAppList/v2/",
   STEAM_GET_FRIEND_NAME:
-    "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=A22C81E13075DA81B066A63FFA475674&steamids="
+    "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=A22C81E13075DA81B066A63FFA475674&steamids="
 };
